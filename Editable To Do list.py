c394ce5ac0bbedbb00de30fc6a7a5c77703b1ccc
WhatusernameE <123575484+WhatusernameE@users.noreply.github.com>
2023-05-10 08:19:27 -0600

    Update Editable To Do list.py

--- Editable To Do list.py
+++ Editable To Do list.py
@@ -1,27 +1,62 @@
 # Build a program that lets you create a to do list. It should let you add and remove items from the list
-#
 
-todo = []
 
-user = input("What chores do you need to do?")
+def remove():
 
-if user:
-    todo.append(user)
-    print(todo)
-    complete = input("Did you complete a chore?")
+    todo.remove(user)
 
+
+def extend():
+
+    todo.append(userThree)
+    
+    
+todo = ['Sweeping', 'Dishes', 'Vacuuming', 'Kitchen Table']
+print(todo)
+
+while True:
+    user = input("What chore do you want to do?")
+    print('Your chore is: ', user)
+
+    userTwo = input("\nWould you like to add another chore?")
+    if userTwo == 'yes':
+        
+        userThree = input("\nAdd your chore: ")
+        print('\nYour extra chore is: ', userThree)
+        extend()
+        print(todo)
+
+    complete = input("\nDid you complete original chore? ")
+    
     if complete == "yes":
-
-        chore = input("Which chore?")
-        if chore == "Sweeping":
-            todo = ["Dishes, Vacuuming"]
+    
+        if user == "Sweeping":
+            remove()
             print(todo)
-        elif chore == "Dishes":
-            todo = ["Sweeping", "Vacuuming"]
+            
+        elif user == "Dishes":
+            remove()
             print(todo)
-        elif chore == "Vacuuming":
-            todo = ["Sweeping", "Dishes"]
+            
+        elif user == "Vacuuming":
+            remove()
             print(todo)
+            
+        elif user == "Kitchen Table":
+            remove()
+            print(todo)
+            
+        elif user == userThree:
+            remove()
+            print(todo)
+            
         else:
             print("Stupid")
             quit()
+    else:
+        print("You cant spell: Yes")
+
+    if todo == []:
+        print('You completed your chores!')
+        print("Congrats!")
+        quit()
